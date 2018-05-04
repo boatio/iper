ec9d5da043c8f10cda9a7228bf544a3de2840844
boatio <37399578+boatio@users.noreply.github.com>
2018-05-04 23:26:39 +0900

    Update IPER(py3).py

--- IPER(py3).py
+++ IPER(py3).py
@@ -1,7 +1,7 @@
 import json
 import socket
 from urllib.request import urlopen
-print("""
+print("""	
                      .####.########..########.########.
                      ..##..##.....##.##.......##.....##
                      ..##..##.....##.##.......##.....##
@@ -9,18 +9,23 @@ print("""
                      ..##..##........##.......##...##..
                      ..##..##........##.......##....##.
                      .####.##........########.##.....##
-                          make by Boat on Boat
-                                 ver 1.0
+			made by Boat on Boat
+				ver 1.0
 """)
 c = None
 while 1:
-        c = input('(I P E R):')
+        c = input('\033[1m\033[32m(I P E R):\033[0m\033[37m')
         if c.startswith('ipw'):
                 c = c.replace('ipw ','')
                 try:
                         print(socket.gethostbyname(c))
                 except:
                         print('URL 가 유효하지 않습니다')
+        elif c.startswith('help'):
+            print("\033[1m\033[36m                   --------------------------------------")
+            print("                   ip [IP] Get ip information")
+            print("                   ipw[Domain]Get domain Ip")
+            print("                   --------------------------------------\033[0m\033[37m")
         elif c.startswith('ip'):
                 c = c.replace('ip ','')
                 try:
